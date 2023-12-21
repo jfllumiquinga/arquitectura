d685503af23bd63a332f52bf6bb19178125ff17d
jonathanFabl <65546725+jonathanFabl@users.noreply.github.com>
2023-12-20 19:21:26 -0500

    ejemplo

---
+++ src/main/java/ec/edu/espe/examenllumiquinga/examenllumiquinga/ExamenllumiquingaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examenllumiquinga.examenllumiquinga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenllumiquingaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenllumiquingaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/examenllumiquinga/examenllumiquinga/ExamenllumiquingaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examenllumiquinga.examenllumiquinga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenllumiquingaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
